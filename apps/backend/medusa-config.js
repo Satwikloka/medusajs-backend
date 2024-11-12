dff0fbc849e95a1831a732ee6a8e10b9428bad25
PavanKalisetti <itspavan09@gmail.com>
2024-11-12 20:55:58 +0530

    local running code with deployment changes

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -5,9 +5,21 @@ dotenv.config({ path: ".env.local" });
 const { QUOTE_MODULE } = require("./src/modules/quote");
 const { loadEnv, defineConfig, Modules } = require("@medusajs/framework/utils");
 
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
+
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: DATABASE_URL,
+    database_extra: { ssl: { rejectUnauthorized: false } },
+    redis_url: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
