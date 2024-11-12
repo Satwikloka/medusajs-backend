d7fa36eb1c3e0015784e51b3b0e5da9e4f492c2b
PavanKalisetti <itspavan09@gmail.com>
2024-11-12 22:05:56 +0530

    deploying in railway

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -1,25 +1,24 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: ".env.local" });
-
-// Rest of your config
 const { QUOTE_MODULE } = require("./src/modules/quote");
 const { loadEnv, defineConfig, Modules } = require("@medusajs/framework/utils");
 
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
+loadEnv(process.env.NODE_ENV, process.cwd());
 
-const DATABASE_URL = 
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+const plugins = [
+  // ...
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      // other options...
+    },
+  },
+]
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: DATABASE_URL,
-    database_extra: { ssl: { rejectUnauthorized: false } },
-    redis_url: process.env.REDIS_URL,
+    databaseUrl: process.env.DATABASE_URL,
+    worker_mode: process.env.MEDUSA_WORKER_MODE,
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
